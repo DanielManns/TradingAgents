4ff944ad70a6aec560844606022c5ff5bc25bf75
dmanns <info@daniel-manns.com>
2026-03-23 17:02:22 +0100

    fix: fixed main

--- tradingagents/dataflows/alpha_vantage.py
+++ tradingagents/dataflows/alpha_vantage.py
@@ -1,1 +1,10 @@
-# Import functions from specialized modules
+# Re-export functions from specialized alpha_vantage modules
+from .alpha_vantage_fundamentals import (  # noqa: F401
+    get_balance_sheet,
+    get_cashflow,
+    get_fundamentals,
+    get_income_statement,
+)
+from .alpha_vantage_indicator import get_indicator  # noqa: F401
+from .alpha_vantage_news import get_global_news, get_insider_transactions, get_news  # noqa: F401
+from .alpha_vantage_stock import get_stock  # noqa: F401
